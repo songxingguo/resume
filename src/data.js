bc73d5cbfd26edd03d6d86bf074be103e2f3fe02
songxingguo <13289899422.qq.com>
2020-06-18 08:35:56 +0800

    feat: 提取数据

---
+++ src/data.js
@@ -0,0 +1,85 @@
+module.exports = {
+  header: {
+    name: '宋兴国',
+    position: '前端开发工程师',
+    telNum: '18983642737',
+    email: 'xg.song@qq.com',
+    wxChat: 's1328989942',
+    blog: 'www.songxingguo.com',
+    blogUrl: 'https://www.songxingguo.com',
+    headIcon: 'https://graphbed.qiniu.songxingguo.com/resume/header.png'
+  },
+  edu: {
+    title: '教育经历',
+    desc: '编织有艺术感的代码',
+    list: [{
+      name: '重庆理工大学',
+      descList: ['描述'],
+      logoUrl: 'https://generative-placeholders.glitch.me/image?width=100&height=100&style=cellular-automata&cells=10',
+      term: '2015.9-2019.6',
+      major: '软件工程'
+    }, {
+      name: '重庆理工大学',
+      descList: ['描述'],
+      logoUrl: 'https://generative-placeholders.glitch.me/image?width=100&height=100&style=cellular-automata&cells=10',
+      term: '2015.9-2019.6',
+      major: '软件工程'
+    }]
+  },
+  tech: {
+    title: '精通前端，涉猎后端',
+    desc: '编织有艺术感的代码',
+    list: ['JavaScript', 'HTML', 'CSS'],
+    descList: ['负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。']
+  },
+  projects: {
+    title: '丰富的项目经验',
+    desc: '业余项目与公司项目相得益彰',
+    list: [{
+      name: '锦鲤多多',
+      desc: '2018.12-至今',
+      tagList: ['HTML5', 'CSS'],
+      content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      link: {
+        name: '项目地址',
+        url: 'https://www.songxingguo.com/'
+      },
+      imgUrl: 'https://generative-placeholders.glitch.me/image?width=600&height=300&style=cellular-automata&cells=10'
+    }, {
+      name: '知晓理工',
+      desc: '2018.12-至今',
+      tagList: ['HTML5', 'CSS'],
+      content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。'
+    }]
+  },
+  works: {
+    title: '3 年工作经验',
+    desc: '曾在知名互联网公司任职',
+    list: [{
+      name: '锦鲤多多',
+      desc: '2018.12-至今',
+      content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      logoUrl: 'https://generative-placeholders.glitch.me/image?width=100&height=100&style=cellular-automata&cells=10',
+      date: {
+        start: '2018/06',
+        end: ''
+      }
+    }, {
+      name: '知晓理工',
+      desc: '2018.12-至今',
+      content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
+      logoUrl: 'https://generative-placeholders.glitch.me/image?width=100&height=100&style=cellular-automata&cells=10',
+      date: {
+        start: '2018/06'
+      }
+    }]
+  },
+  footer: {
+    scanDesc: '查看完整版简历',
+    updatedDesc: '最后更新于2018年9月23日',
+    QRCode: 'https://graphbed.qiniu.songxingguo.com/resume/resume.songxinguo.com.png'
+  }
+}
