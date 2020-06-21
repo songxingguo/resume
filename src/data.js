73b015c76ffa057133224c83c2b9cf9102e1f6be
songxingguo <13289899422.qq.com>
2020-06-21 11:01:47 +0800

    feat: 添加真实数据

--- src/data.js
+++ src/data.js
@@ -14,8 +14,8 @@ module.exports = {
     desc: '编织有艺术感的代码',
     list: [{
       name: '重庆理工大学',
-      descList: ['描述'],
-      logoUrl: 'https://generative-placeholders.glitch.me/image?width=100&height=100&style=cellular-automata&cells=10',
+      descList: ['高等数学，c 语言、Java、 数据结 构、计算机网络、线性代数、操作系统、数据 库、网络协议分析、概率论与统计、软件质量 保证与测试、算法、编译原理、设计模式、软件项目管理。'],
+      logoUrl: 'https://graphbed.qiniu.songxingguo.com/resume/test/cqut_logo.png',
       term: '2015.9-2019.6',
       major: '软件工程'
     }, {
@@ -30,10 +30,11 @@ module.exports = {
     title: '精通前端，涉猎后端',
     desc: '编织有艺术感的代码',
     list: ['JavaScript', 'HTML', 'CSS'],
-    descList: ['负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
-      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
-      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
-      '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。']
+    descList: ['本人性格随和乐观，积极向上，喜欢钻研。乐于沟通，易于融入 集体，乐于助人，学习能力较好，注重理论与实践相结合，在实 践中不断提高专业知识。',
+      '熟练掌握 JavaScript、HTML5、CSS3、微信小程序、Node.js 等技术。熟悉 React、 ES6、 Webpack 、JQuery 等技术。',
+      '熟练掌握 AngularJs、Bootstrap 以及 Maven，Git/SVN 等工 具的使用，熟悉 Mockplus、Axure、Photoshop 等工具的使用。',
+      '作为项目组长参与了“新能源物流车综合运营平台”项目的设计、 开发与测试，并参加了校级创业创新大赛，荣获比赛的一等奖;',
+      '独自设计与开发了一款校园微信小程序。']
   },
   projects: {
     title: '丰富的项目经验',
@@ -43,13 +44,13 @@ module.exports = {
       desc: '2018.12-至今',
       tagList: ['微信小程序'],
       content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
-      imgUrl: 'https://graphbed.qiniu.songxingguo.com/resume/JLDD.png'
+      imgUrl: 'https://graphbed.qiniu.songxingguo.com/resume/test/JLDD.png'
     }, {
       name: '知晓理工',
       desc: '2018.12-至今',
       tagList: ['HTML5', 'CSS'],
       content: '负责协助工程师完成数据安全产品的开发、问题解决；根据项目具体要求，承担开发任务，按计划完成任务目标；完成领导交办的其他工作。',
-      imgUrl: 'https://graphbed.qiniu.songxingguo.com/resume/wecqut.jpg'
+      imgUrl: 'https://graphbed.qiniu.songxingguo.com/resume/test/cqut.png'
     }, {
       name: '相册',
       desc: '2018.12-至今',
