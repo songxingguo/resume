603b9f87d5308cc68bda133e679a89ad9d704429
songxingguo <xg.song@qq.com>
2023-02-04 09:49:20 +0800

    feat: 配置rem

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const postCssPxToRem = require("postcss-pxtorem");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 22, // 1rem的大小
+          propList: ["*"], // 需要转换的属性，这里选择全部都进行转换
+        }),
+      ],
+    },
+  },
+});
