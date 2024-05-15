d20cdb6f5f14e6df13be865ef70238045530ce00
songxingguo <1328989942@qq.com>
2024-05-15 12:40:55 +0000

    Update Blog By GithubAction With Build

---
+++ index.160a7338.js
@@ -0,0 +1,2 @@
+const os=navigator.userAgent.toLowerCase().indexOf("micromessenger");document.querySelectorAll("a").forEach((function(e){return e.setAttribute("target","_blank")})),window.printResume=function(){0<os?alert("微信无法打印，请使用手机浏览器访问"):window.print()};
+//# sourceMappingURL=index.160a7338.js.map
